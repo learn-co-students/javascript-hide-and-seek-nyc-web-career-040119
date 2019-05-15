4462ee6ab4a7788a6698b27b61ed0c2fdaa1a833
axionos <axionoso@gmail.com>
2019-05-15 09:16:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+};
 
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+};
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div');
+};
+
+function increaseRankBy(n){
+
+  const rankedLists = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0; i < rankedLists.length; i++) {
+    let children = rankedLists[i].children;
+
+    for (let j = 0; j < children.length; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+};
